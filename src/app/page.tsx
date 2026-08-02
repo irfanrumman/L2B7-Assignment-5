6868bf62995c9c2b947f860b68ccd92afe2dd997
irfanrumman <mdirfansayid@gmail.com>
2026-08-02 08:36:51 +0600

    fix: home page funtional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,15 @@
-import {getAllPropertiesAction} from "./{public}/_actions/HomePageActions";
-import {HeroSection} from "./{public}/_components/HeroSection";
+import { getFeaturedPropertiesAction } from "./{public}/_actions/HomePageActions";
+import { HeroSection } from "./{public}/_components/HeroSection";
 import { FeaturedProperties } from "./{public}/_components/FeaturedProperties";
 import { CTASection } from "./{public}/_components/CTASection";
-import { Suspense } from "react";
-import { getAllProperties } from "../utils/getAllProperties";
 
 export default async function HomePage() {
-  // const properties = await getAllPropertiesAction();
-  const propertiesData = await getAllProperties();
-  console.log(propertiesData.data);
+  const result = await getFeaturedPropertiesAction();
 
   return (
     <>
       <HeroSection />
-      
-       <Suspense fallback={<div className="text-center text-muted-foreground">Loading...</div>}>
-          <FeaturedProperties properties={propertiesData.data} />
-        </Suspense>
+      <FeaturedProperties properties={result.success ? result.data : []} />
       <CTASection />
     </>
   );
